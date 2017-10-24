e199260e4fd7d02eb98b8b09b4e102fc6263d106
jordaner <ericjames1495@gmail.com>
2017-10-24 13:18:40 +0100

    Moving forward
    SUM datasets sorted, cross validation sorted, chunking sorted.

---
+++ LinearRegression-SUMwithNoise.py
@@ -0,0 +1,55 @@
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split, KFold,cross_val_score
+from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+import numpy as np
+
+features = ['Feature 1','Feature 2','Feature 3','Feature 4','Feature 5 (meaningless but please still use it)',
+            'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10']
+samples_sizes= [100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000,50000000,100000000]
+
+i = 0
+while i<len(samples_sizes):
+
+    df = pd.read_csv("C:\\Users\\ericj\\PycharmProjects\\Assignment1\\The SUM dataset, with noise.csv",sep=";",nrows = samples_sizes[i])
+
+    X = df.loc[:,features]
+    y = df["Noisy Target"]
+
+    #X_train, X_test, y_train, y_test, = train_test_split(X,y,test_size=0.3)
+
+    #print(X_train.shape, y_train.shape)
+    #print(X_test.shape, y_test.shape)
+
+    lm = linear_model.LinearRegression()
+
+    #model = lm.fit(X_train,y_train)
+    #predictions = lm.predict(X_test)
+
+    #print(predictions[0:5])
+
+    NMSE_results= cross_val_score(lm,X,y,cv=10,scoring="neg_mean_squared_error") # Choose another regression metric
+
+    NMSE_results = NMSE_results * -1
+
+    print(NMSE_results)
+
+    NMSE_results = np.sqrt(NMSE_results)
+
+    print(NMSE_results)
+
+    i += 1
+    # The coefficients
+    #print('With Noise Coefficients: \n', lm.coef_)
+    #    The mean squared error
+    #print("With Noise Mean squared error: %.2f"
+     #     % mean_squared_error(y_test, predictions))
+    # Explained variance score: 1 is perfect prediction
+    #print('With Noise Variance score: %.2f' % r2_score(y_test, predictions))
+
+    ## The line / model
+    #plt.scatter(y_test, predictions)
+    #plt.xlabel('True Values')
+    #plt.ylabel('Predictions')
+
+    #print ('Score:', model.score(X_test, y_test))
---
+++ SUMwithoutNoise.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+
+features = ['Feature 1','Feature 2','Feature 3','Feature 4','Feature 5 (meaningless but please still use it)',
+            'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10']
+i = 500
+while i<=500000000:
+    # Load in the data with `read_csv()`
+    df = pd.read_csv("C:\\Users\\ericj\\PycharmProjects\\Assignment1\\The SUM dataset, without noise.csv",sep=";",nrows = i)
+
+    X = df.loc[:,features]
+    y = df.Target
+
+    X_train, X_test, y_train, y_test, = train_test_split(X,y,test_size=0.3)
+
+    print(X_train.shape, y_train.shape)
+    print(X_test.shape, y_test.shape)
+
+    lm = linear_model.LinearRegression()
+
+    model = lm.fit(X_train,y_train)
+    predictions = lm.predict(X_test)
+
+    #print(predictions[0:5])
+
+    # The coefficients
+    print('Without Noise Coefficients: \n', lm.coef_)
+    #    The mean squared error
+    print("Without Noise Mean squared error: %.2f"
+          % mean_squared_error(y_test, predictions))
+    # Explained variance score: 1 is perfect prediction
+    print('Without Noise Variance score: %.2f' % r2_score(y_test, predictions))
+
+    ## The line / model
+    #plt.scatter(y_test, predictions)
+    #plt.xlabel('True Values')
+    #plt.ylabel('Predictions')
+
+    print ('Score:', model.score(X_test, y_test))
+
+    i = i*10
