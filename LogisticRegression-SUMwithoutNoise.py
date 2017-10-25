d2d8193999eddceb61e120fb432e49ce80de5ac5
mloughma <33071595+mloughma@users.noreply.github.com>
2017-10-25 16:00:41 +0100

    deleted logistic
    yes

--- LogisticRegression-SUMwithoutNoise.py
+++
@@ -1,6 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-from sklearn.preprocessing import LabelEncoder
-
-encoder = LabelEncoder()
-df["target_class"] = encoder.fit_transform(df["Target Class"])
