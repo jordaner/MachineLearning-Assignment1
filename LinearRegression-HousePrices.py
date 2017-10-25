7dd272007086bee28fbda91d938de2771a2354bf
mloughma <33071595+mloughma@users.noreply.github.com>
2017-10-25 20:55:05 +0100

    ok
    ok

--- LinearRegression-HousePrices.py
+++ LinearRegression-HousePrices.py
@@ -37,7 +37,7 @@ samples_sizes= [100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,1000
 i = 0
 while i<len(samples_sizes):
 
-    df = pd.read_csv("/Users/markloughman/Desktop/Machine Learning/DATA/housing dataset.csv",sep=",",nrows = samples_sizes[i])
+    df = pd.read_csv("path",sep=",",nrows = samples_sizes[i])
     X = df.loc[:,features]
  #   print(X)
     y = df.SalePrice
