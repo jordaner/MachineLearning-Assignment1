8b8d5ffc133b2b17ddefce3f01cfda55aecc0d53
mloughma <33071595+mloughma@users.noreply.github.com>
2017-10-25 17:18:48 +0100

    Prec Score
    Prec Score

--- LogisticRegression-SUMwithNoise.py
+++ LogisticRegression-SUMwithNoise.py
@@ -4,7 +4,10 @@ from sklearn.model_selection import cross_val_score,KFold
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
+from sklearn.metrics import precision_score
+from sklearn.metrics import fbeta_score, make_scorer
 
+#wine and housing data set tbh
 
 features = ['Feature 1','Feature 2','Feature 3','Feature 4','Feature 5 (meaningless but please still use it)',
             'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10']
@@ -14,7 +17,7 @@ le = preprocessing.LabelEncoder()
 i = 0
 while i<len(samples_sizes):
 
-    df = pd.read_csv("path",sep=";",nrows = samples_sizes[i])
+    df = pd.read_csv("/Users/markloughman/Desktop/Machine Learning/DATA/TheSumDataSetWithNoise",sep=";",nrows = samples_sizes[i])
 
     catnum = df["Noisy Target Class"].tolist()
 
@@ -28,8 +31,14 @@ while i<len(samples_sizes):
     kfold = KFold(n_splits=10,random_state=0)
     ACC_results = cross_val_score(lr, X, y_b, cv=kfold, scoring="accuracy")
 
+    PREC_scorer =  make_scorer(precision_score, average="weighted")
+    PREC_results = cross_val_score(lr, X, y_b, cv=kfold, scoring=PREC_scorer)
+
     mean_ACC = ACC_results.mean()
+    mean_PREC = PREC_results.mean()
 
     print("Accuracy with sample of size of ", samples_sizes[i], " = ", mean_ACC)
+    print("Precision Score with sample of size of ", samples_sizes[i], " = ", mean_PREC)
+
 
     i += 1
--- LogisticRegression-SUMwithoutNoise.py
+++ LogisticRegression-SUMwithoutNoise.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import cross_val_score,KFold
 from sklearn import preprocessing
 import pandas as pd
 import numpy as np
-
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.metrics import precision_score
 
 features = ['Feature 1','Feature 2','Feature 3','Feature 4','Feature 5 (meaningless but please still use it)',
             'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10']
@@ -14,7 +15,7 @@ le = preprocessing.LabelEncoder()
 i = 0
 while i<len(samples_sizes):
 
-    df = pd.read_csv("path",sep=";",nrows = samples_sizes[i])
+    df = pd.read_csv("/Users/markloughman/Desktop/Machine Learning/DATA/TheSumDataSetWithoutNoise",sep=";",nrows = samples_sizes[i])
 
     catnum = df["Target Class"].tolist()
 
@@ -28,8 +29,13 @@ while i<len(samples_sizes):
     kfold = KFold(n_splits=10,random_state=0)
     ACC_results = cross_val_score(lr, X, y_b, cv=kfold, scoring="accuracy")
 
+    PREC_scorer =  make_scorer(precision_score, average="weighted")
+    PREC_results = cross_val_score(lr, X, y_b, cv=kfold, scoring=PREC_scorer)
+
     mean_ACC = ACC_results.mean()
+    mean_PREC = PREC_results.mean()
 
     print("Accuracy with sample of size of ", samples_sizes[i], " = ", mean_ACC)
+    print("Precision Score with sample of size of ", samples_sizes[i], " = ", mean_PREC)
 
     i += 1
