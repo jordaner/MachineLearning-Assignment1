c7405f799978cd43c987462c79028d6dca18e737
Bernard Ryan <bearnard.oriain@gmail.com>
2017-10-26 09:47:54 +0100

    KNN house proces
    26.10, 9:47
    Making progress on this one, adding to an example I found and working on extending it.

---
+++ KNN-HousePrices.py
@@ -0,0 +1,18 @@
+from sklearn import neighbors, datasets, preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd
+import scipy.stats as stats
+housing = pd.read_csv("C:\\Users\\Bernard\\Dropbox\\TCD\\CS4404\\Machine Learning Datasets\\House Prices\\housing dataset.csv",sep=";",nrows=10)
+X, y = housing.data[:, :2], housing.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33)
+scaler = preprocessing.StandardScaler().fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+print("Accuracy score ")
+print(accuracy_score(y_test, y_pred))
+print("Classification report ")
+print(classification_report(y_test, y_pred))
