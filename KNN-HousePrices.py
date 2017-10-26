90c715c4445ead5be2cb07e114e272b5699099bc
Bernard Ryan <bearnard.oriain@gmail.com>
2017-10-26 12:41:36 +0100

    House Prices version 2

--- KNN-HousePrices.py
+++ KNN-HousePrices.py
@@ -1,18 +1,50 @@
-from sklearn import neighbors, datasets, preprocessing
+from sklearn import neighbors
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score as accuracy
 import pandas as pd
-import scipy.stats as stats
-housing = pd.read_csv("C:\\Users\\Bernard\\Dropbox\\TCD\\CS4404\\Machine Learning Datasets\\House Prices\\housing dataset.csv",sep=";",nrows=10)
-X, y = housing.data[:, :2], housing.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33)
-scaler = preprocessing.StandardScaler().fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-knn = neighbors.KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
-y_pred = knn.predict(X_test)
-print("Accuracy score ")
-print(accuracy_score(y_test, y_pred))
-print("Classification report ")
-print(classification_report(y_test, y_pred))
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+def trans_col(column):
+    transformed_list = LabelEncoder().fit_transform(column.tolist())
+    transformed_series = pd.Series(data=transformed_list)
+    transformed_series = pd.Series(data=transformed_list)
+    transformed_series.replace(np.NaN, 0)
+    transformed_series.set_value(100, 2)
+    return transformed_series
+
+features = ['MSSubClass',	'MSZoning',	'LotFrontage',	'LotArea',	'Street',
+            'Alley',	'LotShape',	'LandContour',	'Utilities',	'LotConfig',
+            'LandSlope',	'Neighborhood',	'Condition1',	'Condition2',	'BldgType',
+            'HouseStyle',	'OverallQual',	'OverallCond',
+            'YearBuilt',	'YearRemodAdd',	'RoofStyle',	'RoofMatl',
+            'Exterior1st',	'Exterior2nd',	'MasVnrType',	'MasVnrArea',	'ExterQual',
+            'ExterCond',	'Foundation',	'BsmtQual',	'BsmtCond',	'BsmtExposure',
+            'BsmtFinType1',	'BsmtFinSF1',	'BsmtFinType2',	'BsmtFinSF2',
+            'BsmtUnfSF',	'TotalBsmtSF',	'Heating',	'HeatingQC',	'CentralAir',
+            'Electrical',	'1stFlrSF',	'2ndFlrSF',	'LowQualFinSF',	'GrLivArea',	'BsmtFullBath',
+            'BsmtHalfBath',	'FullBath',	'HalfBath',	'BedroomAbvGr',	'KitchenAbvGr',
+            'KitchenQual',	'TotRmsAbvGrd',	'Functional',	'Fireplaces',	'FireplaceQu',	'GarageType',
+            'GarageYrBlt',	'GarageFinish'	'GarageCars',	'GarageArea',
+            'GarageQual',	'GarageCond',	'PavedDrive',	'WoodDeckSF',	'OpenPorchSF',
+            'EnclosedPorch',	'3SsnPorch',	'ScreenPorch',	'PoolArea',	'PoolQC',	'Fence',
+            'MiscFeature',	'MiscVal',	'MoSold',	'YrSold',	'SaleType',	'SaleCondition']
+samples_sizes= [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000]
+i = 0
+
+while i < len(samples_sizes):
+    houses = pd.read_csv("C://Users//Bernard//Dropbox//TCD//CS4404//Machine Learning Datasets//House Prices//housing dataset.csv",sep=",",nrows = samples_sizes[i])
+    X, y = houses.loc[:,features], houses.SalePrice
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 33)
+    for column in X:
+        if "object" in str(X[column].dtype):
+            X[column] = trans_col(X[column])
+    X = X.replace(np.nan, 0)
+    knn = neighbors.KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    # accuracy metrics
+    y_pred = knn.predict(X_test)
+    knn.score(X_test, y_test)
+    print(accuracy(y_test, y_pred))
+    # classification report
+    i += 1
