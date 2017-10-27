afd43620b2747b9987c7a70dbd822152592b2d57
Bernard Ryan <bearnard.oriain@gmail.com>
2017-10-27 07:53:55 +0100

    Ridge regression updated
    Updated and fixed metrics for RR in both house prices and SUM with noise. Standardized both files so both files have minimal crossover.

--- Task3/RidgeRegression-HousePrices.py
+++ Task3/RidgeRegression-HousePrices.py
@@ -1,19 +1,16 @@
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split, KFold,cross_val_score
-from sklearn.metrics import mean_squared_error, r2_score
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn import linear_model
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 
-
-def transformColumn(column):
-    transformed_list = LabelEncoder().fit_transform(column.tolist())
-    transformed_series = pd.Series(data=transformed_list)
-    transformed_series = pd.Series(data=transformed_list)
-    transformed_series.replace(np.NaN, 0)
-    transformed_series.set_value(100, 2)
-    return transformed_series
-
+def trans_col(column):
+    trans_list = LabelEncoder().fit_transform(column.tolist())
+    trans_series = pd.Series(data=trans_list)
+    trans_series.replace(np.NaN, 0)
+    trans_series.set_value(100, 2)
+    return trans_series
 
 features = ['MSSubClass',	'MSZoning',	'LotFrontage',	'LotArea',	'Street',
             'Alley',	'LotShape',	'LandContour',	'Utilities',	'LotConfig',
@@ -31,75 +28,24 @@ features = ['MSSubClass',	'MSZoning',	'LotFrontage',	'LotArea',	'Street',
             'GarageQual',	'GarageCond',	'PavedDrive',	'WoodDeckSF',	'OpenPorchSF',
             'EnclosedPorch',	'3SsnPorch',	'ScreenPorch',	'PoolArea',	'PoolQC',	'Fence',
             'MiscFeature',	'MiscVal',	'MoSold',	'YrSold',	'SaleType',	'SaleCondition']
-
 samples_sizes= [100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000,50000000,100000000]
-
 i = 0
-while i<len(samples_sizes):
 
-    df = pd.read_csv("/Users/markloughman/Desktop/Machine Learning/DATA/housing dataset.csv",sep=",",nrows = samples_sizes[i])
-    X = df.loc[:,features]
- #   print(X)
-    y = df.SalePrice
-
-    #X_train, X_test, y_train, y_test, = train_test_split(X,y,test_size=0.3)
-
-    #print(X_train.shape, y_train.shape)
-    #print(X_test.shape, y_test.shape)
-
-
-
-   # encoder = LabelEncoder()
-    #df["target_class"] = encoder.fit_transform(df["SalePrice"].tolist())
-
-
-
-    lm = linear_model.Ridge(normalize=True)
-
+while i < len(samples_sizes):
+    data = pd.read_csv("C://Users//Bernard/Dropbox//TCD//CS4404//Machine Learning Datasets//House Prices/housing dataset.csv",sep=",",nrows = samples_sizes[i])
+    X = data.loc[:,features]
+    y = data.SalePrice
+    lin_mod = linear_model.Ridge(normalize = True)
     for column in X:
-        if "object" in str(X[column].dtype):
-            X[column] = transformColumn(X[column])
+        if "object" in str(X[column].dtype): X[column] = trans_col(X[column])
     X = X.replace(np.nan,0)
-
-
-    #model = lm.fit(X_train,y_train)
-    #predictions = lm.predict(X_test)
-
-    #print(predictions[0:5])
-
-    NMSE_results= cross_val_score(lm,X,y,cv=10,scoring="neg_mean_squared_error") # Choose another regression metric
-
+    NMSE_results= cross_val_score(lin_mod, X, y, cv = 10, scoring = "neg_mean_squared_error") # Choose another regression metric
     NMSE_results = NMSE_results * -1
-
     RMS_results = np.sqrt(NMSE_results)
-
     mean_error = RMS_results.mean()
-
-
-    abs_mean_error = cross_val_score(lm,X,y,cv=10,scoring="neg_mean_absolute_error")
+    abs_mean_error = cross_val_score(lin_mod, X, y, cv = 10, scoring = "neg_mean_absolute_error")
     abs_mean_error = abs_mean_error * -1
     abs_mean_error = abs_mean_error.mean()
-
-
     print("Error with sample size of ", samples_sizes[i], "for mean squared error = ", mean_error)
-
     print("Error with sample size of ", samples_sizes[i], "for absolute mean error =", abs_mean_error)
-
-
-
-
     i += 1
-    # The coefficients
-    #print('With Noise Coefficients: \n', lm.coef_)
-    #    The mean squared error
-    #print("With Noise Mean squared error: %.2f"
-     #     % mean_squared_error(y_test, predictions))
-    # Explained variance score: 1 is perfect prediction
-    #print('With Noise Variance score: %.2f' % r2_score(y_test, predictions))
-
-    ## The line / model
-    #plt.scatter(y_test, predictions)
-    #plt.xlabel('True Values')
-    #plt.ylabel('Predictions')
-
-    #print ('Score:', model.score(X_test, y_test))
--- Task3/RidgeRegression-SUMwithNoise.py
+++ Task3/RidgeRegression-SUMwithNoise.py
@@ -1,41 +1,24 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import mean_squared_error, r2_score
-import pandas as pd
 
-features = ['Feature 1','Feature 2','Feature 3','Feature 4','Feature 5 (meaningless but please still use it)',
-            'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10']
-#columns = "Feature 1 Feature 2 Feature 3 Feature 4 Feature 5 Feature 6 Feature 7 Feature 8 Feature 9 Feature 10".split()
-samples_sizes= [100,500,1000,5000,10000,50000,100000,500000,1000000,5000000,10000000,50000000,100000000]
-
+features = ['Feature 1','Feature 2','Feature 3','Feature 4','Feature 5 (meaningless but please still use it)', 'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10']
+samples_sizes = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000]
 i = 0
-while i<len(samples_sizes):
-    # Load in the data with `read_csv()`
-    df = pd.read_csv("/Users/markloughman/Desktop/Machine Learning/DATA/TheSumDataSetWithNoise",sep=";",nrows = samples_sizes[i])
 
-    X = df.loc[:,features]
-    y = df["Noisy Target"]
-
-    regressionmetric = "neg_mean_squared_error"
-
-    lm = linear_model.Ridge(normalize=True)
-
-    NMSE_results= cross_val_score(lm,X,y,cv=10,scoring="neg_mean_squared_error") # Choose another regression metric
-
+while i < len(samples_sizes):
+    data = pd.read_csv("C://Users//Bernard/Dropbox//TCD//CS4404//Machine Learning Datasets//The SUM dataset//with noise//The SUM dataset, with noise.csv", sep=";" , nrows = samples_sizes[i])
+    X, y = data.loc[:,features], data["Noisy Target"]
+    reg_metric = "neg_mean_squared_error"
+    lin_mod = linear_model.Ridge(normalize=True)
+    NMSE_results= cross_val_score(lin_mod, X, y, cv=10, scoring=reg_metric) # Choose another regression metric
     NMSE_results = NMSE_results * -1
-
     RMS_results = np.sqrt(NMSE_results)
-
     mean_error = RMS_results.mean()
-
-    abs_mean_error = cross_val_score(lm, X, y, cv=10, scoring="neg_mean_absolute_error")
+    abs_mean_error = cross_val_score(lin_mod, X, y, cv=10, scoring=reg_metric)
     abs_mean_error = abs_mean_error * -1
     abs_mean_error = abs_mean_error.mean()
-
     print("Error with sample size of ", samples_sizes[i], "for mean squared error = ", mean_error)
-
     print("Error with sample size of ", samples_sizes[i], "for absolute mean error =", abs_mean_error)
-
     i += 1
